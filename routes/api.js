6ecc7aca53d00109d3038ffa2784d98c9b3c2694
Cory Benton <corybenton@gmail.com>
2023-06-14 11:51:59 -0400

    api mostly working

---
+++ routes/api.js
@@ -0,0 +1,46 @@
+const api = require('express').Router();
+const notedb = require('../db/db.json');
+const fs = require('fs');
+const uuid = require('../helpers/uuid');
+
+api.get('/api/notes', (req, res) => res.json(notedb));
+
+api.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            review_id: uuid(),
+        };
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const currentdb = JSON.parse(data);
+                currentdb.push(newNote);
+
+                fs.writeFile('./db/db.json',
+                    JSON.stringify(currentdb, null, 4),
+                    (writeErr) => writeErr
+                        ? console.error(writeErr)
+                        : console.info("Success")
+                )
+            }
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        res.json(response);
+    }
+});
+
+api.delete('/api/notes/:id', (req, res) =>
+    console.log("Working on it.")
+);
+
+module.exports = api;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/api.js');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', api);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => 
